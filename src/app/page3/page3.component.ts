76be40fbfda407ebb3d72b7254427b0af6b6a96c
EngiBeerHub <engibeer0422@gmail.com>
2023-06-17 14:29:56 +0900

    added page3

--- src/app/page3/page3.component.ts
+++ src/app/page3/page3.component.ts
@@ -3,18 +3,18 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-page2',
-  templateUrl: './page2.component.html',
-  styleUrls: ['./page2.component.scss'],
+  selector: 'app-page3',
+  templateUrl: './page3.component.html',
+  styleUrls: ['./page3.component.scss'],
 })
-export class Page2Component {
+export class Page3Component {
   constructor(private router: Router, private location: Location) {}
 
   navigateToPage3() {
     this.router.navigate(['/page3']);
   }
+
   goBack() {
     this.location.back();
   }
-
 }
